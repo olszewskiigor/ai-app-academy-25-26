2abec0466f4ea8871e52e222581eb78bed707077
Igor Olszewski <71021482+olszewskiigor@users.noreply.github.com>
2025-10-28 18:47:20 +0100

    Update script.js

--- 28-10-25/script.js
+++ 28-10-25/script.js
@@ -6,5 +6,15 @@ let timer = null;
 
 // Funkcja, która uruchamia stoper
 function startStoper(){
-    
+    // Sprawdzamy, czy timer jest pusty (żeby nie uruchomić dwóch stoperów naraz)
+    if(timer == null)
+    {
+        // Ustawiamy licznik, który co 1s (1000 milisekund) wykona podany kod
+        timer = setInterval(() => {
+            // Zwiększamy liczbę sekund o 1
+            sekund++;
+            // Szukamy na stronie elementu o id "czas" i zmianiamy go na wartość sekund
+            document.getElementById("czas").textContent = sekund;
+        }, 1000); // 1000 milisekund = 1 sekunda
+    }
 }
