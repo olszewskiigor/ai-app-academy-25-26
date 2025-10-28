c2b79e0d46430429b7085b0841d4597b33075d45
Igor Olszewski <71021482+olszewskiigor@users.noreply.github.com>
2025-10-28 18:42:17 +0100

    Update script.js

---
+++ 28-10-25/script.js
@@ -0,0 +1,10 @@
+// Zmienna, w której będziemy zapisywać, ile sekund minęło
+let sekund = 0;
+
+// Zmienna, w której będziemy przechowywać "timer" - czyli licznik czasu
+let timer = null;
+
+// Funkcja, która uruchamia stoper
+function startStoper(){
+    
+}
